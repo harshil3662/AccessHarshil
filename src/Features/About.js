57b4f940a45917c85c283fb04a21b65be78e0ff8
harshil3662 <vaghaniharshil11291@gmail.com>
2024-04-10 21:28:09 -0400

    working on home and about page

--- src/Features/About.js
+++ src/Features/About.js
@@ -5,16 +5,16 @@ function About() {
   return (
     <div class="container about">
 
-      <div className='p-5 head d-flex justify-content-center align-items-center'>About Me</div>
+      <div className='p-5 page-heading d-flex justify-content-center align-items-center'>About Me</div>
 
       <div className='col'>
 
-        <div className='row introduction p-5'>
-          <div className='p-2 intro-head d-flex justify-content-start align-items-center'>Intoduction</div>
+        <div className='row box p-5'>
+          <div className='p-2 head d-flex justify-content-start align-items-center'>Intoduction</div>
           <div className='row'>
             <div className='container d-flex justify-content-center align-items-center'>
-              <div className='introduction-box'>
-                <div className='intro ps-4 pt-1'>I'm Harshil Vaghani, a seasoned web developer with over 5 to 6 years of experience specializing in
+              <div className='introduction'>
+                <div className='lines ps-4 pt-1'>I'm Harshil Vaghani, a seasoned web developer with over 5 to 6 years of experience specializing in
                   designing and developing websites and web applications for diverse purposes. Throughout my career, I've honed my skills in
                   various web development technologies, including React.js, Node.js, and more. My expertise lies in crafting innovative digital
                   solutions that not only meet but exceed client expectations. I'm dedicated to staying updated with the latest trends and
@@ -30,14 +30,14 @@ function About() {
           </div>
         </div>
 
-        <div className='rov experience p-5 mt-5'>
-          <div className='p-2 experience-head d-flex justify-content-start align-items-center'>Experience</div>
+        <div className='row box p-5 mt-5'>
+          <div className='p-2 head d-flex justify-content-start align-items-center'>Experience</div>
           <div className='row'>
             <div className='container d-flex justify-content-center align-items-center'>
-              <div className='experience-box'>
+              <div className='experience'>
                 <div className='ps-4 pt-1'>
                   <div className='mb-2'><span className='company-name'>Software Lab</span><br/><span className='timeline'>Jun 2021 - Apr 2022</span></div>
-                  <div className='exp'>
+                  <div className='lines'>
                     Design and develop robust backend components for Python applications and APIs using Django and Flask, ensuring efficient and
                     scalable solutions. Implement data fetching mechanisms from databases and third-party APIs to enrich and optimise functionality
                     within existing applications. Actively participate in debugging and troubleshooting processes to identify and address issues promptly, ensuring
@@ -52,7 +52,7 @@ function About() {
         
         <div className='row'>
           <div className='container d-flex justify-content-center align-items-center'>
-            <a className='cv mt-5' href='https://drive.google.com/file/d/1HNVyHXM4Q87DApEYk9HLuwZmW7F5U_yQ/view?usp=sharing'>My Resume</a>
+            <a className='cv-btn mt-5' href='https://drive.google.com/file/d/1HNVyHXM4Q87DApEYk9HLuwZmW7F5U_yQ/view?usp=sharing'>My Resume</a>
           </div>
         </div>
       </div>
--- src/Features/Home.js
+++ src/Features/Home.js
@@ -10,60 +10,71 @@ function Home() {
 
   return (
     <div className='container d-flex justify-content-center justify-content-sm-center home'>
-      <div className='row m-5'>
-        <div className='mt-4 col1 col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 order-2 order-sm-2 order-xs-2 order-md-2
-         order-lg-1 order-xl-1 order-xxl-1'>
-          <div className='row hv-home-col1-div1 m-5 text-center text-sm-center text-md-center text-lg-start text-xl-start'>Hello! I'm Harshil Vaghani.</div>
-          <div className='row hv-home-col1-div2 m-5 text-center  text-sm-center text-md-center text-lg-start text-xl-start'>I'm here to innovate web development
-            turning ideas into creative and real solutions.
-          </div>
-          <div className='m-5 text-center text-sm-center text-md-center text-lg-start text-xl-start'>
-            <Link to={"/about"}>
-              <button className='hv-btn'>About Me</button>
-            </Link>
-          </div>
-          <div className='m-5 d-flex justify-content-center justify-content-sm-center justify-content-md-center 
-            justify-content-lg-start justify-content-xl-start align-items-center'>
-              <div className='row links'>
-                <div className='col'>
-                  <div className='row'>
-                    <div className='col'>
-                      <a href='https://github.com/harshil3662' onMouseOver={()=>{setHover1('hovered')}} onMouseOut={()=>{setHover1('')}}>
-                        {hover1 ? <img className='link-img' src='github1.png' alt='github'/> : <img className='link-img' src='github.png' alt='github'/>}
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className='col'>
-                  <div className='row'>
-                    <div className='col'>
-                      <a href='https://www.linkedin.com/in/harshil-vaghani/' onMouseOver={()=>{setHover2('hovered')}} onMouseOut={()=>{setHover2('')}}>
-                        {hover2 ? <img className='link-img' src='linkedin1.png' alt='linkedin'/> : <img className='link-img' src='linkedin.png' alt='linkedin'/>}
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className='col'>
-                  <div className='row'>
-                    <div className='col'>
-                      <a href='https://www.instagram.com/_harshilvaghani_/' onMouseOver={()=>{setHover3('hovered')}} onMouseOut={()=>{setHover3('')}}>
-                        {hover3 ? <img className='link-img' src='instagram1.png' alt='instagram'/> : <img className='link-img' src='instagram.png' alt='instagram'/>}
-                      </a>
-                    </div>
-                  </div>
-                </div>
+
+      <div className="col-12">
+
+        <div className='m-5 text-center text-sm-center text-md-center d-flex justify-content-center justify-content-sm-center headline'>
+          Web app developer & MERN stack developer
+        </div>
+
+        <div className='row'>
+
+          <div className='mt-4 col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 order-2 order-sm-2 order-xs-2 order-md-2
+          order-lg-1 order-xl-1 order-xxl-1 d-flex justify-content-center justify-content-sm-center'>
+            <div className="m-5 col1">
+              <div className='hv-home-col1-div1 m-5 text-center text-sm-center text-md-center text-lg-start text-xl-start'>Hello! I'm<br/><label className='name'>Harshil Vaghani.</label></div>
+              <div className='row hv-home-col1-div2 m-5 text-center  text-sm-center text-md-center text-lg-start text-xl-start'>I'm here to innovate web development
+                turning ideas into creative and real solutions.
               </div>
+              <div className='m-5 text-center text-sm-center text-md-center text-lg-start text-xl-start'>
+                <Link to={"/about"}>
+                  <button className='hv-btn'>About Me</button>
+                </Link>
+              </div>
+              <div className='m-5 d-flex justify-content-center justify-content-sm-center justify-content-md-center 
+                justify-content-lg-start justify-content-xl-start align-items-center'>
+                  <div className='row links'>
+                    <div className='col'>
+                      <div className='row'>
+                        <div className='col'>
+                          <a href='https://github.com/harshil3662' onMouseOver={()=>{setHover1('hovered')}} onMouseOut={()=>{setHover1('')}}>
+                            {hover1 ? <img className='link-img' src='github1.png' alt='github'/> : <img className='link-img' src='github.png' alt='github'/>}
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='col'>
+                      <div className='row'>
+                        <div className='col'>
+                          <a href='https://www.linkedin.com/in/harshil-vaghani/' onMouseOver={()=>{setHover2('hovered')}} onMouseOut={()=>{setHover2('')}}>
+                            {hover2 ? <img className='link-img' src='linkedin1.png' alt='linkedin'/> : <img className='link-img' src='linkedin.png' alt='linkedin'/>}
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='col'>
+                      <div className='row'>
+                        <div className='col'>
+                          <a href='https://www.instagram.com/_harshilvaghani_/' onMouseOver={()=>{setHover3('hovered')}} onMouseOut={()=>{setHover3('')}}>
+                            {hover3 ? <img className='link-img' src='instagram1.png' alt='instagram'/> : <img className='link-img' src='instagram.png' alt='instagram'/>}
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className='col d-flex justify-content-center align-items-center order-lg-2 order-xl-2 order-xxl-2'>
-          <div className='vertical-line'></div>
-        </div>
-        <div className='col-12 col-sm-12 col-md-12 col-lg-5 col-xl-5 col-xxl-5 
-          d-flex justify-content-center align-items-center order-1 order-sm-1 order-xs-1 order-md-1 order-lg-3 order-xl-3 order-xxl-3'>
-          <div className='row circle'>
-            <img id="avatar" src='avatar.png' alt='avatar'/>
+
+          <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 
+            d-flex justify-content-center align-items-center order-1 order-sm-1 order-xs-1 order-md-1 order-lg-3 order-xl-3 order-xxl-3'>
+            <div className='row circle'>
+              <img id="avatar" src='avatar.png' alt='avatar'/>
+            </div>
           </div>
+
         </div>
+
       </div>
     </div>
   )
