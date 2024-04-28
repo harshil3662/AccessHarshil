311cf511ba815d584655ebcbee7b853b62f39065
harshil3662 <vaghaniharshil11291@gmail.com>
2024-04-28 14:17:45 -0400

    project page almost done

--- src/Features/Projects.js
+++ src/Features/Projects.js
@@ -5,27 +5,50 @@ function Projects() {
   return (
     <div className='container projects'>
       <div className='text-center heading mt-5'>Projects</div>
-        <div className='row d-flex justify-content-center align-items-start mt-5'>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>Repairmate IOS Application</div>
-          </div>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>Repairmate Web Application</div>
-          </div>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>My Portfolio</div>
-          </div>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>Chainlink with ethers.js</div>
-          </div>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>Crypto Wallet</div>
-          </div>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>Docker & Kubernetes</div>
-          </div>
-          <div className='col-5 col-sm-5 col-md-5 project m-3 p-2'>
-            <div>Hands-on DevOps Experience</div>
+        <div className='row d-flex justify-content-center align-items-center mt-5'>
+          <div className='container d-inline-flex justify-content-center align-items-center flex-wrap'>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="ios-featured.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>IOS Application</div>
+            </div>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="mern-1.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>MERN Application</div>
+            </div>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="portfolio.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>My Portfolio</div>
+            </div>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="ethers.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>ethers.js</div>
+            </div>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="wallet.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>Crypto Wallet</div>
+            </div>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="dk2.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>Docker & Kubernetes</div>
+            </div>
+            <div className='project m-3 p-3'>
+              <div className='image-box'>
+                <img src="devops.png" className='img' alt="..."/>
+              </div>
+              <div className='project-name mt-3'>Hands-On DevOps</div>
+            </div>
           </div>
         </div>
     </div>
--- src/Features/Skills.js
+++ src/Features/Skills.js
@@ -6,7 +6,7 @@ function Skills() {
     <div className='container skills'>
       <div className='text-center heading mt-5'>Skills</div>
       <div className='row d-flex justify-content-center align-items-stretch mt-5'>
-        <div className='col-12 col-sm-12 col-md-5 col-lg-2 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
+        <div className='col-12 col-sm-12 col-md-5 col-lg-5 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
           <div className='skill-secction-name text-center m-4'>Frontend</div>
           <div className='row m-4'>
             <div className='container-fluid d-flex justify-content-start align-items-center flex-wrap'>
@@ -18,10 +18,10 @@ function Skills() {
             </div>
           </div>
         </div>
-        <div className='col-12 col-sm-12 col-md-5 col-lg-2 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
+        <div className='col-12 col-sm-12 col-md-5 col-lg-5 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
           <div className='skill-secction-name text-center m-3'>Backend</div>
           <div className='row m-4'>
-            <div className='container-fluid d-flex justify-content-start align-items-center flex-wrap'>
+            <div className='container-fluid d-flex justify-content-xxl-start justify-content-xl-center align-items-center flex-wrap'>
               <div className='skill-name'>Node Js</div>
               <div className='skill-name'>Express Js</div>
               <div className='skill-name'>web3.js</div>
@@ -30,17 +30,17 @@ function Skills() {
             </div>
           </div>
         </div>
-        <div className='col-12 col-sm-12 col-md-5 col-lg-2 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
+        <div className='col-12 col-sm-12 col-md-5 col-lg-5 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
           <div className='skill-secction-name text-center m-3'>Database</div>
           <div className='row m-4'>
-            <div className='container-fluid d-flex justify-content-start align-items-center flex-wrap'>
+            <div className='container-fluid d-flex justify-content-xxl-start justify-content-xl-center align-items-center flex-wrap'>
               <div className='skill-name'>MySql</div>
               <div className='skill-name'>MongoDB</div>
               <div className='skill-name'>PostgreSGL</div>
             </div>
           </div>
         </div>
-        <div className='col-12 col-sm-12 col-md-5 col-lg-2 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
+        <div className='col-12 col-sm-12 col-md-5 col-lg-5 col-xl-2 col-xxl-2 flex-fill skill-box m-3 p-2'>
           <div className='skill-secction-name text-center m-3'>DevOps</div>
           <div className='row m-4'>
             <div className='container-fluid d-flex justify-content-start align-items-center flex-wrap'>
